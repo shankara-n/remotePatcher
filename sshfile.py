241b9218966edea3ae61be537b0221e3d34c7ec8
Feyaz Baker <44121644+NegaMage@users.noreply.github.com>
2020-01-28 07:44:31 +0530

    Contains sensitive info
    The login is hard-coded, the password isn't sensitive but this is Tharun's account. This is on NITK net's private server.

---
+++ sshfile.py
@@ -0,0 +1,6 @@
+import paramiko
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname='10.4.1.15', username='s1221', password='testinglol')
+stdin,stdout,stderr=ssh_client.exec_command("ls")
+print(stdout)
