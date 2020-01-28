f545f330d026a571853ab9d32910c9bdb4931c33
Feyaz Baker <44121644+NegaMage@users.noreply.github.com>
2020-01-28 07:51:38 +0530

    Formatting stuff

--- sshfile.py
+++ sshfile.py
@@ -3,4 +3,8 @@ ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname='10.4.1.15', username='s1221', password='testinglol')
 stdin,stdout,stderr=ssh_client.exec_command("ls")
-print(stdout)
+
+lines = []
+lines = stdout.readlines()
+for line in lines:
+    print(line, end='')
