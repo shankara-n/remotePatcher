58f7128146c69614e4d992e2baeb77933951451b
Feyaz <feyaz.181co119@nitk.edu.in>
2020-01-29 20:49:08 +0530

    Added files to remotepatcher Same files as mongoDiagnostic library Didn't know if I should update only to that branch, or if I had to put here as well, so I put here too.

--- sshfile.py
+++ sshfile.py
@@ -1,10 +1,29 @@
 import paramiko
+import csv
+filepointer = open('newFile', 'r')
+contents = csv.reader(filepointer)
+row = next(contents)
+filepointer.close()
+# print(type(row))
+# print(row[0])
+# print(row[1])
+# print(row[2])
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname='10.4.1.15', username='s1221', password='testinglol')
-stdin,stdout,stderr=ssh_client.exec_command("ls")
+ssh_client.connect(hostname=row[0], username=row[1], password=row[2])
+stdin,stdout,stderr=ssh_client.exec_command("ls | grep 'new'")
 
 lines = []
 lines = stdout.readlines()
 for line in lines:
     print(line, end='')
+
+ftp_client = ssh_client.open_sftp()
+ftp_client.put('newFile', 'newFile')
+ftp_client.close()
+
+ftp_client=ssh_client.open_sftp()
+ftp_client.get('hello', 'warpath')
+ftp_client.close()
+
+stdin,stdout,stderr=ssh_client.exec_command("ls | grep new")
