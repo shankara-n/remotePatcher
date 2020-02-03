a5cc11b12493a1a409973aa125891a2056a25517
Vignesh Srinivasan <vignesh1722@gmail.com>
2020-02-03 18:07:19 +0530

    update readIPaddress.py

--- readIPaddress.py
+++ readIPaddress.py
@@ -13,23 +13,23 @@ def ping(host):
     command = ['ping', param, '1', host]
     return subprocess.call(command) == 0
 
-with open('Patch Automation - IPs CSV.csv', mode='r') as csv_file:
-    IPlist=[]
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count==0:
-            line_count+=1
-            continue
+csv_file=open('Patch Automation - IPs CSV.csv', mode='r')
+IPlist=[]
+csv_reader = csv.reader(csv_file, delimiter=',')
+line_count = 0
+for row in csv_reader:
+    if line_count==0:
+        line_count+=1
+        continue
+    else:
+        connectionStatus = ping(row[0])
+        print(connectionStatus)
+        if(row[0]==''):
+            print('No IP found', row[0])
+        elif(connectionStatus==False):
+            print('Bad IP address', row[0])
         else:
-            result = ping(row[0])
-            print(result)
-            if(row[0]==''):
-                print('No IP found', row[0])
-            elif(result==False):
-                print('Bad IP address', row[0])
-            else:
-                IPlist.append(row[0])
-                print('Successfully connected to IP ',row[0])
+            IPlist.append(row[0])
+            print('Successfully connected to IP ',row[0])
 print(IPlist)
         
