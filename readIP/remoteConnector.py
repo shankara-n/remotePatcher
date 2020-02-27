78d55f8af7538b218386f1715dfd32e15d21bb34
Vignesh Srinivasan <vignesh1722@gmail.com>
2020-02-27 15:35:21 +0530

    added feature to check for no file, empty file, lesser wait time, and minimal output

--- readIP/remoteConnector.py
+++ readIP/remoteConnector.py
@@ -1,6 +1,7 @@
 import csv
 import platform
 import subprocess
+import os
 
 def ping(host):
     """
@@ -9,27 +10,43 @@ def ping(host):
     """
     # Option for the number of packets as a function of
     param = '-n' if platform.system().lower()=='windows' else '-c'
-    # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host]
+    # Building the command. Ex: "ping -c 1 google.com -q"
+    command = ['ping', param, '1', '-W 100',  '-Q', host]
     return subprocess.call(command) == 0
 
-with open('Patch Automation - IPs CSV.csv', mode='r') as csv_file:
-    IPlist=[]
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count==0:
-            line_count+=1
+file_name="../input/Patch Automation - IPs CSV.csv"
+
+#checking if the file exists and was opened properly
+try:
+    csv_file=open(file_name, mode='r')
+except IOError:
+    print('File does not exist')
+    exit()
+
+
+
+goodIPlist=[]
+badIPlist=[]
+csv_reader = csv.reader(csv_file, delimiter=',')
+line_count = 0
+
+for row in csv_reader:
+    if line_count==0:
+        line_count+=1
+        continue
+    else:
+        if not row:
             continue
+        connectionStatus = ping(row[0])
+        if(connectionStatus==False):
+            badIPlist.append(row[0])
         else:
-            result = ping(row[0])
-            print(result)
-            if(row[0]==''):
-                print('No IP found', row[0])
-            elif(result==False):
-                print('Bad IP address', row[0])
-            else:
-                IPlist.append(row[0])
-                print('Successfully connected to IP ',row[0])
-print(IPlist)
-        
+            goodIPlist.append(row[0])
+
+#checking if the file is empty
+if(line_count==0 or 1):
+    print("File is empty.") 
+    exit()
+
+print('Good IP list:', goodIPlist)
+print('Bad IP list:', badIPlist)
