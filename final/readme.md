c134ccc8d776ba8b902ac5556157ba049ae1871e
Feyaz <feyaz.181co119@nitk.edu.in>
2020-03-01 15:45:05 +0530

    Made more changes, added diagnostic script

---
+++ final/readme.md
@@ -0,0 +1,19 @@
+# Implementation script for Shashank's diagnostic script
+
+## Execution
+
+`python3 final.py`
+
+## Notes
+
+Ensure the csv files are in input folder, and named accordingly.
+
+Ensure diagnostic.py is in script folder
+
+## Issues
+
+- [ ] Ping command keeps flooding the terminal with output, find a quieter version
+
+- [ ] Somebody please collaborate this is really draining
+
+- [ ] Not done yet
--- final/test.py
+++ final/test.py
@@ -1,5 +1,52 @@
-l1 = [1,2,3]
-l2 = [3,2,1]
+import csv
+import platform
+import subprocess
+import os
 
-for (i, j) in zip(l1, l2):
-    print("{} and {}".format(i,j))
+def ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    # Building the command. Ex: "ping -c 1 google.com -q"
+    command = ['ping', param, '1', '-W 100',  '-Q', host]
+    return subprocess.call(command) == 0
+
+file_name="../input/Patch Automation - IPs CSV.csv"
+
+#checking if the file exists and was opened properly
+try:
+    csv_file=open(file_name, mode='r')
+except IOError:
+    print('File does not exist')
+    exit()
+
+
+
+goodIPlist=[]
+badIPlist=[]
+csv_reader = csv.reader(csv_file, delimiter=',')
+line_count = 0
+
+for row in csv_reader:
+    if line_count==0:
+        line_count+=1
+        continue
+    else:
+        if not row:
+            continue
+        connectionStatus = ping(row[0])
+        if(connectionStatus==False):
+            badIPlist.append(row[0])
+        else:
+            goodIPlist.append(row[0])
+
+#checking if the file is empty
+if(line_count==0 or 1):
+    print("File is empty.") 
+    exit()
+
+print('Good IP list:', goodIPlist)
+print('Bad IP list:', badIPlist)
